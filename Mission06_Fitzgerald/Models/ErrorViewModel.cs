046060109b576395dc19e93740dd2ee0cc6b3de4
Preston Fitzgerald <prestonfitz@gmail.com>
2024-02-12 10:10:39 -0700

    Add project files.

---
+++ Mission06_Fitzgerald/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission06_Fitzgerald.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
